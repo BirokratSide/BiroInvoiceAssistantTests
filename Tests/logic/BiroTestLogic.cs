51c949ed3853d46969d3457f941c0dc75c5c8558
mirceta <kristijan.mirceta@gmail.com>
2018-07-23 15:10:24 +0200

    Implemented logic for birotest

--- Tests/logic/BiroTestLogic.cs
+++ Tests/logic/BiroTestLogic.cs
@@ -17,16 +17,43 @@ namespace Tests.logic
 
         public void DeleteAllRecordsFromDatabase() {
             // remove all partners
-
+            context.Partner.RemoveRange(context.Partner);
             // remove all options
+            context.CrmstrankeOpcije.RemoveRange(context.CrmstrankeOpcije);
         }
 
         public void InsertPartner(string sifra, string davcnaStevilka) {
-            // insert the specified partner
+            Partner partner = new Partner();
+            partner.Sifra = sifra;
+            partner.DavcnaStevilka = davcnaStevilka;
+            context.Add(partner);
+            context.SaveChanges();
         }
 
         public void InsertOpcija(string sifra, bool rih, string rihd, float rihk, float rihz, int rihc, string rihp) {
             // insert all of these options for the specified partner
+            CrmstrankeOpcije opcija = GetOpcija(sifra, "RIH", rih.ToString());
+            context.CrmstrankeOpcije.Add(opcija);
+            opcija = GetOpcija(sifra, "RIHD", rihd);
+            context.CrmstrankeOpcije.Add(opcija);
+            opcija = GetOpcija(sifra, "RIHK", rihk.ToString());
+            context.CrmstrankeOpcije.Add(opcija);
+            opcija = GetOpcija(sifra, "RIHZ", rihz.ToString());
+            context.CrmstrankeOpcije.Add(opcija);
+            opcija = GetOpcija(sifra, "RIHC", rihc.ToString());
+            context.CrmstrankeOpcije.Add(opcija);
+            opcija = GetOpcija(sifra, "RIHP", rihp);
+            context.CrmstrankeOpcije.Add(opcija);
+            context.SaveChanges();
+        }
+
+        private CrmstrankeOpcije GetOpcija(string sifra, string opcijaName, string vrednost) {
+            CrmstrankeOpcije opcija = new CrmstrankeOpcije();
+            opcija.Sifra = sifra;
+            opcija.Aplikacija = "RIH";
+            opcija.Opcija = opcijaName;
+            opcija.Vrednost = vrednost;
+            return opcija;
         }
     }
 }
