3e66105e4915ba225878658a48f8c0775e835e6c
mirceta <kristijan.mirceta@gmail.com>
2018-07-23 15:02:03 +0200

    Birotest logic outlined

--- Tests/logic/BiroTestLogic.cs
+++ Tests/logic/BiroTestLogic.cs
@@ -2,9 +2,31 @@
 using System.Collections.Generic;
 using System.Text;
 
+using Tests.birotest;
+
 namespace Tests.logic
 {
-    class BirotestLogic
+    class BirotestLogic : IBirotestLogic
     {
+
+        BiroInvoiceAssistantTestingOnlyContext context;
+
+        public BirotestLogic() {
+            context = new BiroInvoiceAssistantTestingOnlyContext();
+        }
+
+        public void DeleteAllRecordsFromDatabase() {
+            // remove all partners
+
+            // remove all options
+        }
+
+        public void InsertPartner(string sifra, string davcnaStevilka) {
+            // insert the specified partner
+        }
+
+        public void InsertOpcija(string sifra, bool rih, string rihd, float rihk, float rihz, int rihc, string rihp) {
+            // insert all of these options for the specified partner
+        }
     }
 }
